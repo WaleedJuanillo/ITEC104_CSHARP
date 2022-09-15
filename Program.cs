2705c23b4c03583f276a6d6eb18ca86e546c86bc
WaleedJuanillo <112452406+WaleedJuanillo@users.noreply.github.com>
2022-09-15 16:54:10 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿public class ProgramStruct {
+    struct Employee {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public float completedTraining;
+    }
+    public static void Main(string[] args){
+
+        Employee employee1;
+
+        Console.WriteLine("What is your firstName?");
+        employee1.firstName = Console.ReadLine();
+        Console.WriteLine("What is yourlastName?");
+        employee1.lastName = Console.ReadLine();
+        Console.WriteLine("How old are you?");
+        employee1.age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Do you completed the training?");
+        employee1.completedTraining = float.Parse(Console.ReadLine());
+        Console.WriteLine("You entered");
+        Console.WriteLine("firstName: "+ employee1.firstName);
+        Console.WriteLine("lastName: "+ employee1.lastName);
+        Console.WriteLine("age: "+ employee1.age);
+        Console.WriteLine("completedTraining?: "+ employee1.completedTraining);
+        }
+}
