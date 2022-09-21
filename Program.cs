18d55bf8f3d072dff7869d1d41eb8e8ec3aff325
WaleedJuanillo <112452406+WaleedJuanillo@users.noreply.github.com>
2022-09-21 09:17:44 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
-﻿public class ProgramStruct {
-    struct Employee {
-        public string firstName;
-        public string lastName;
-        public int age;
-        public float completedTraining;
-    }
-    public static void Main(string[] args){
+﻿using System;
 
-        Employee employee1;
+namespace ITEC104_ACT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Emails = { "waleed.pogi025@yahoo.com", 
+                                "juanillowlaeed@yahoo.com", 
+                                "waleed.juanillo69@yahoo.com" };
 
-        Console.WriteLine("What is your firstName?");
-        employee1.firstName = Console.ReadLine();
-        Console.WriteLine("What is yourlastName?");
-        employee1.lastName = Console.ReadLine();
-        Console.WriteLine("How old are you?");
-        employee1.age = int.Parse(Console.ReadLine());
-        Console.WriteLine("Do you completed the training?");
-        employee1.completedTraining = float.Parse(Console.ReadLine());
-        Console.WriteLine("You entered");
-        Console.WriteLine("firstName: "+ employee1.firstName);
-        Console.WriteLine("lastName: "+ employee1.lastName);
-        Console.WriteLine("age: "+ employee1.age);
-        Console.WriteLine("completedTraining?: "+ employee1.completedTraining);
+            string[] Usernames = {  "waleed022", 
+                                    "wjuanillo069", 
+                                    "Jwaleed025" };
+
+            string[] Passwords = { "TBH569", 
+                                   "tinining0123", 
+                                   "parak66" };
+
+            Console.Write("Index : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Email    : " + Emails[x]);
+            Console.WriteLine("Username : " + Usernames[x]);
+            Console.WriteLine("Password : " + Passwords[x]);       
+
         }
+    }
 }
+
